5f523765ce095d8f86495a793cac829aa3e70b82
Ununennium111 <isaac.cv123@gmail.com>
2022-05-28 16:12:32 -0400

    feat: image controllers created
    the controller "image-controllers.js" has been created.
    this file contains the following controllers: createImage, getUserImages.

---
+++ controllers/image-controllers.js
@@ -0,0 +1,32 @@
+const Image = require('../models/image-model');
+
+const createImage = async (req, res) => {
+    req.body.uploadedBy = req.user.userId;
+
+    const image = await Image.create(req.body);
+
+    return res
+        .status(201)
+        .json(
+            {
+                image
+            }
+        );
+}
+
+const getUserImages = async (req, res) => {
+    const images = await Image.find({ upladedBy: req.userId });
+
+    return res
+        .status(200)
+        .json(
+            {
+                images
+            }
+        );
+}
+
+module.exports = {
+    createImage,
+    getUserImages
+}
